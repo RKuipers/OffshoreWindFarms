117fba4db1931f4636ccd909e8ad86315d25b405
RKuipers <robin_kuipers93@hotmail.com>
2019-11-05 21:07:35 +0000

    Create knapsack.py

---
+++ Python/knapsack.py
@@ -0,0 +1,26 @@
+def knapSack(budget, costs, values, n):
+
+	if budget == 0 or n == 0:
+		return []
+	
+	if (costs[n-1] > budget):
+		return knapSack(budget, costs, values, n-1)
+	
+	incl = knapSack(budget - costs[n-1], costs, values, n-1)
+	incl.append(n-1)
+	excl = knapSack(budget, costs, values, n-1)
+	
+	ival = values[n-1] + checkVal(incl, values)
+	eval = checkVal(excl, values)
+	
+	if ival > eval:
+		return incl
+	else:
+		return excl
+		
+
+def checkVal(indices, values):
+	sum = 0
+	for i in indices:
+		sum += values[i]		
+	return sum
