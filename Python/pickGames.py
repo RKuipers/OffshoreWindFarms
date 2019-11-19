f1544d787c24341dbaaa24dc7d6c0aa0daf5793b
RKuipers <robin_kuipers93@hotmail.com>
2019-11-19 16:42:07 +0000

    Updated python

--- Python/pickGames.py
+++ Python/pickGames.py
@@ -71,6 +71,64 @@ def knapSackDP(budget, costs, values, n):
     
     return result
 		
+def voteSat(budget, n):
+    vals = []
+    games = []
+    for i in range(0, n):
+        vals.append([-1] * (budget+1))
+        games.append([[]] * (budget+1))
+    
+    def m(i, j):
+        if j <= 0:
+            return 0
+        
+        if i == 0:
+            if prices[i] > j:
+                return 0
+            else:
+                games[i][j] = [names[i]]
+                #print("Calling CALCVAL1 on: " + str([names[i]]))
+                return calcVal([names[i]], votesets)
+        
+        if(vals[i-1][j] == -1):
+            vals[i-1][j] = m(i-1, j)
+        
+        chosen = False
+        
+        if prices[i] > j:
+            vals[i][j] = vals[i-1][j]
+        else:
+            if(vals[i-1][j-prices[i]] == -1):
+                vals[i-1][j-prices[i]] = m(i-1, j-prices[i])
+            newGameList = games[i-1][j-prices[i]].copy()
+            newGameList.append(names[i])
+            #print("Calling CALCVAL2 on: " + str(newGameList))
+            newVal = calcVal(newGameList, votesets)
+            if(vals[i-1][j] > newVal):
+                vals[i][j] = vals[i-1][j]
+            else:
+                vals[i][j] = newVal
+                chosen = True
+        
+
+        if chosen:
+            games[i][j] = newGameList
+        else:
+            games[i][j] = games[i-1][j].copy()
+        return vals[i][j]
+    
+    m(n-1, budget)
+    return games[n-1][budget]
+
+def calcVal(setOfGames, setOfVotes):
+    numSat = 0
+    for vote in setOfVotes:
+        satisfied = False
+        for game in vote:
+            satisfied |= (game in setOfGames)
+        if satisfied:
+            numSat += 1
+    return numSat
 
 def checkVal(indices, values):
 	s = 0
@@ -82,19 +140,12 @@ def openFile(fn, s):
     df = pd.read_excel(fn, s)
     return df.iloc[0:,2]
 
-def getData(file_name, sheet):
-    column = openFile(file_name, sheet)
-
-    for cell in column:
-        games = cell.split(", ")
-        for game in games:
-            if game in names:
-                votes[names.index(game)] += 1
-            else:
-                price = ra.randrange(15, 50)
-                ind = insort(prices, price)
-                names.insert(ind, game)
-                votes.insert(ind, 1)
+def getData(file_name, sheet1, sheet2):
+    df = pd.read_excel(file_name, sheet1)
+    names.extend([j for i in pd.DataFrame(df, columns= ['Name']).values.tolist() for j in i])
+    votes.extend([j for i in pd.DataFrame(df, columns= ['Votes']).values.tolist() for j in i])
+    prices.extend([j for i in pd.DataFrame(df, columns= ['Price']).values.tolist() for j in i])
+    votesets.extend([i[0].split(', ') for i in pd.read_excel(file_name, sheet2).values.tolist()])
                 
 def insort(l, x):
     i = 0
@@ -107,25 +158,29 @@ def run(budget):
     print("")
     start = dt.datetime.now()   
     #games = knapSack(budget, prices, votes, len(names))
-    games = knapSackDP(budget, prices, votes, len(names))
+    #games = knapSackDP(budget, prices, votes, len(names))
+    games = voteSat(budget, len(names))
     #print("DP RESULT: " + str([names[i] for i in gamesDP]) + " therefore same? " + str(games == gamesDP))
     print("Budget: " + str(budget) + "...................................................")
-    print(str([names[i] for i in games]))
-    print("Total: " + str(sum([prices[i] for i in games])))
+    print(str(games))
+    print("Total: " + str(sum([prices[names.index(i)] for i in games])))
+    print("Satisfied voters: " + str(calcVal(games, votesets)))
     end = dt.datetime.now() - start
     print("Duration: " + str(end))
 
-
 ra.seed(42)
 
-file_name = r'test.xlsx'
-sheet = "Sheet1"
+file_name = r'results.xlsx'
+sheet1 = "Sheet1"
+sheet2 = "Sheet2"
 
 names = []
 votes = []
 prices = []
+votesets = []
                 
-getData(file_name, sheet)
+getData(file_name, sheet1, sheet2)
+prices = [round(x) for x in prices]
 for i in range(len(names)):
     print("Name: " + str(names[i]) + " , votes: " + str(votes[i]) + " , price: " + str(prices[i]))
 
