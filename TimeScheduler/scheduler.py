406abf0b437474657b99a919ab6b767ba0a8d062
RKuipers <robin_kuipers93@hotmail.com>
2020-02-20 16:26:39 +0000

    Updated scheduler to have exe

--- TimeScheduler/scheduler.py
+++ TimeScheduler/scheduler.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-from datetime import date
+import datetime as dt
 
-now = datetime.now()
+def chop_microseconds(delta):
+    return delta - dt.timedelta(microseconds=delta.microseconds)
+
+now = dt.datetime.now()
 start = now.time()
 
 print ("What time do you need to be done? (HH:MM)")
 endstr = input()
-end = datetime.strptime(endstr, "%H:%M").time()
-available = datetime.combine(date.today(), end) - datetime.combine(date.today(), start)
-minutes = available.seconds / 60
+end = dt.datetime.strptime(endstr, "%H:%M").time()
+available = dt.datetime.combine(dt.date.today(), end) - dt.datetime.combine(dt.date.today(), start)
+available = chop_microseconds(available)
+minutes = round(available.seconds / 60)
 
 worku = 86
 breaku = 40
 
 blocks = divmod((minutes-worku), (worku + breaku))
+
+print ("Available time: " + str(available) + " (" + str(minutes) + " minutes)")
 print ("Full blocks: ", blocks[0] + 1)
 print ("Leftover minutes: ", blocks[1])
+
+print ("Press Enter to quit")
+_ = input()
