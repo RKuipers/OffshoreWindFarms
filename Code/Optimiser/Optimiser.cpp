4fde6c7fd42a19a1979373159cfb754d98629cff
RKuipers <robin_kuipers93@hotmail.com>
2020-05-21 15:07:23 +0100

    Added n, started converting to expressionless constraints

--- Code/Optimiser/Optimiser.cpp
+++ Code/Optimiser/Optimiser.cpp
@@ -14,9 +14,10 @@ using namespace std;
 using namespace ::dashoptimization;
 
 // Program settings
+#define SEED 42
 #define WEATHERTYPE 1
 #define VERBOSITY 1
-#define NAMES 0
+#define NAMES 1
 #define DATAFILE "installDays.dat"
 #define OUTPUTFILE "install.sol"
 
@@ -57,6 +58,7 @@ XPRBexpr Cos[NPERIODS];
 // Model variables
 XPRBvar O[NPERIODS];
 XPRBvar N[NRES][NPERIODS];
+XPRBvar n[NRES][NTIMES];
 XPRBvar s[NASSETS][NTASKS][NTIMES];
 XPRBvar f[NASSETS][NTASKS][NTIMES];
 
@@ -79,14 +81,16 @@ void generateWeather(ifstream* datafile)
 		int waveHeight[NTIMES];
 	if (WEATHERTYPE == 0)
 	{
-		waveHeight[0] = 120;
-		cout << 0 << ": " << waveHeight[0] << endl;
+		waveHeight[0] = 120; 
+		if (VERBOSITY > 1)
+			cout << 0 << ": " << waveHeight[0] << endl;
 		for (int t = 1; t < NTIMES; ++t)
 		{
 			bonus += (base - waveHeight[t - 1]) / 40;
 
 			waveHeight[t] = max(0, waveHeight[t - 1] + bonus + (rand() % variety));
-			cout << t << ": " << waveHeight[t] << endl;
+			if (VERBOSITY > 1)
+				cout << t << ": " << waveHeight[t] << endl;
 		}
 	}
 	else if (WEATHERTYPE == 1)
@@ -94,11 +98,13 @@ void generateWeather(ifstream* datafile)
 		for (int p = 0; p < NPERIODS; ++p)
 		{
 			waveHeight[p * TPP] = 130;
-			cout << p * TPP << ": " << waveHeight[p * TPP] << endl;
+			if (VERBOSITY > 1)
+				cout << p * TPP << ": " << waveHeight[p * TPP] << endl;
 			for (int t = (p * TPP) + 1; t < (p + 1) * TPP; ++t)
 			{
 				waveHeight[t] = max(0, waveHeight[t - 1] + bonus +  (rand() % variety));
-				cout << t << ": " << waveHeight[t] << endl;
+				if (VERBOSITY > 1)
+					cout << t << ": " << waveHeight[t] << endl;
 			}
 		}
 	}
@@ -222,7 +228,7 @@ int main(int argc, char** argv)
 {
 	int a, p, r, i, x, t, t1, t2, t3;
 
-	srand(42);
+	srand(SEED);
 
 	// Read data from file
 	if (VERBOSITY > 0)
@@ -242,40 +248,30 @@ int main(int argc, char** argv)
 
 	for (p = 0; p < NPERIODS; ++p)
 	{
-		if (NAMES == 0)
-			O[p] = prob.newVar(NULL, XPRB_UI);
-		else
-			O[p] = prob.newVar(("O_" + to_string(p)).c_str(), XPRB_UI);
-
+		O[p] = prob.newVar(("O_" + to_string(p)).c_str(), XPRB_UI);
 		O[p].setLB(0);
 
 		for (r = 0; r < NRES; ++r)
 		{
-			if(NAMES == 0)
-				N[r][p] = prob.newVar(NULL, XPRB_UI);
-			else
-				N[r][p] = prob.newVar(("N_" + to_string(r) + " " + to_string(p)).c_str(), XPRB_UI);
+			N[r][p] = prob.newVar(("N_" + to_string(r) + "_" + to_string(p)).c_str(), XPRB_UI);
 			N[r][p].setLB(0);
 			N[r][p].setUB(m[r][p]);
 		}
 	}
 
 	// Create the timestep-based decision variables
-	for (a = 0; a < NASSETS; ++a)
-		for (i = 0; i < NTASKS; ++i)
-			for (t = 0; t < NTIMES; ++t)
+	for (t = 0; t < NTIMES; ++t)
+	{
+		for (r = 0; r < NRES; ++r)
+			n[r][t] = prob.newVar(("n_" + to_string(r) + "_" + to_string(t)).c_str(), XPRB_UI);
+
+		for (a = 0; a < NASSETS; ++a)
+			for (i = 0; i < NTASKS; ++i)
 			{
-				if (NAMES == 0)
-				{
-					s[a][i][t] = prob.newVar(NULL, XPRB_BV);
-					f[a][i][t] = prob.newVar(NULL, XPRB_BV);
-				}
-				else
-				{
-					s[a][i][t] = prob.newVar(("s_" + to_string(a) + "_" + to_string(i) + "_" + to_string(t)).c_str(), XPRB_BV);
-					f[a][i][t] = prob.newVar(("f_" + to_string(a) + "_" + to_string(i) + "_" + to_string(t)).c_str(), XPRB_BV);
-				}
+				s[a][i][t] = prob.newVar(("s_" + to_string(a) + "_" + to_string(i) + "_" + to_string(t)).c_str(), XPRB_BV);
+				f[a][i][t] = prob.newVar(("f_" + to_string(a) + "_" + to_string(i) + "_" + to_string(t)).c_str(), XPRB_BV);
 			}
+	}
 
 	//--------------------------------Objective--------------------------------
 	if (VERBOSITY > 0)
@@ -303,10 +299,11 @@ int main(int argc, char** argv)
 				TaskStart[a][i] += s[a][i][t];
 				TaskFinish[a][i] += f[a][i][t];
 			}
+
 			if (NAMES == 0)
 			{
-				prob.newCtr(NULL, TaskStart[a][i] == 1);
-				prob.newCtr(NULL, TaskFinish[a][i] == 1);
+				prob.newCtr(TaskStart[a][i] == 1);
+				prob.newCtr(TaskFinish[a][i] == 1);
 			}
 			else
 			{
@@ -327,7 +324,7 @@ int main(int argc, char** argv)
 					Prec[a][x][t1][t2] = s[a][j][t1] + f[a][i][t2];
 
 					if (NAMES == 0)
-						prob.newCtr(NULL, Prec[a][x][t1][t2] <= 1);
+						prob.newCtr(Prec[a][x][t1][t2] <= 1);
 					else
 						prob.newCtr(("Prec_" + to_string(a) + "_" + to_string(x) + "_" + to_string(t1) + "_" + to_string(t2)).c_str(), Prec[a][x][t1][t2] <= 1);
 				}
@@ -345,7 +342,7 @@ int main(int argc, char** argv)
 					Weat[a][i][t1][t2] += (f[a][i][t2] + s[a][i][t1]) * 0.5 * weather - d[i] * (s[a][i][t1] + f[a][i][t2] - 1);
 
 					if (NAMES == 0)
-						prob.newCtr(NULL, Weat[a][i][t1][t2] >= 0);
+						prob.newCtr(Weat[a][i][t1][t2] >= 0);
 					else
 						prob.newCtr(("Weat_" + to_string(a) + "_" + to_string(i) + "_" + to_string(t1) + "_" + to_string(t2)).c_str(), Weat[a][i][t1][t2] >= 0);
 				}
@@ -355,31 +352,61 @@ int main(int argc, char** argv)
 		for (p = 0; p < NPERIODS; ++p)
 			for (t = p * TPP; t < (p + 1) * TPP; ++t)
 			{
-				for (a = 0; a < NASSETS; a++)
+				XPRBctr ctr;
+				if (t == 0)
+				{
+					if (NAMES == 0)
+						ctr = prob.newCtr(n[r][t] == 0);
+					else
+						ctr = prob.newCtr(("Nee_" + to_string(r) + "_" + to_string(t)).c_str(), n[r][t] == 0);
+
 					for (i = 0; i < NTASKS; ++i)
 					{
-						Res[r][p][t - p * TPP] += rho[r][i] * s[a][i][0];
-						for (t1 = 1; t1 <= t; ++t1)
-							Res[r][p][t - p * TPP] += rho[r][i] * (s[a][i][t1] - f[a][i][t1 - 1]);
+						if (rho[r][i] == 0)
+							continue;
+
+						for (a = 0; a < NASSETS; a++)
+							ctr.addTerm(s[a][i][t], -rho[r][i]);
 					}
+				}
+				else
+				{
+					if (NAMES == 0)
+						ctr = prob.newCtr(n[r][t] - n[r][t - 1] == 0);
+					else
+						ctr = prob.newCtr(("Nee_" + to_string(r) + "_" + to_string(t)).c_str(), n[r][t] - n[r][t - 1] == 0);
+
+					for (i = 0; i < NTASKS; ++i)
+					{
+						if (rho[r][i] == 0)
+							continue;
+
+						for (a = 0; a < NASSETS; a++)
+						{
+							ctr.addTerm(s[a][i][t], -rho[r][i]);
+							ctr.addTerm(f[a][i][t-1], rho[r][i]);
+						}
+					}
+				}
 
 				if (NAMES == 0)
-					prob.newCtr(NULL, Res[r][p][t - p * TPP] <= N[r][p]);
+					ctr = prob.newCtr(N[r][p] >= n[r][t]);
 				else
-					prob.newCtr(("Res_" + to_string(r) + "_" + to_string(p) + "_" + to_string(t)).c_str(), Res[r][p][t - p * TPP] <= N[r][p]);
+					ctr = prob.newCtr(("Hav_" + to_string(r) + "_" + to_string(p) + "_" + to_string(t)).c_str(), N[r][p] >= n[r][t]);
 			}
 
 	// Online turbines link
 	for (p = 0; p < NPERIODS; ++p)
 	{
+		XPRBctr ctr;
+		if (NAMES == 0)
+			ctr = prob.newCtr(NULL, O[p] == 0);
+		else
+			ctr = prob.newCtr(("Onl_" + to_string(p)).c_str(), O[p] == 0);
+
 		for (t = 0; t < p * TPP; ++t)
 			for (a = 0; a < NASSETS; a++)
-				Onl[p] += f[a][NTASKS - 1][t];
-
-		if (NAMES == 0)
-			prob.newCtr(NULL, Onl[p] == O[p]);
-		else
-			prob.newCtr(("Onl_" + to_string(p)).c_str(), Onl[p] == O[p]);
+				ctr.addTerm(f[a][NTASKS - 1][t], -1);
 	}
 
 	double duration = ((double)clock() - start) / (double)CLOCKS_PER_SEC;
@@ -390,7 +417,8 @@ int main(int argc, char** argv)
 	if (VERBOSITY > 0)
 		cout << "Solving problem" << endl;
 	start = clock();
-	//prob.setMsgLevel(1);
+	if (VERBOSITY == 0)
+		prob.setMsgLevel(1);
 	prob.setSense(XPRB_MAXIM);
 	prob.exportProb(XPRB_LP, "Install1");
 	prob.mipOptimize("");
@@ -417,6 +445,9 @@ int main(int argc, char** argv)
 	cout << "Resources needed per period and type: " << endl;
 	for (p = 0; p < NPERIODS; ++p)
 	{
+		if (VERBOSITY > 1)
+			cout << "Period ";
+
 		int v = round(N[0][p].getSol());
 		cout << p << ": " << v;
 		file << "N_0_" << p << ": " << v << endl;;
@@ -428,6 +459,24 @@ int main(int argc, char** argv)
 			file << "N_" << r << "_" << p << ": " << v << endl;;
 		}
 		cout << endl;
+
+		if (VERBOSITY > 1)
+		{
+			for (t = p * TPP; t < (p + 1) * TPP; ++t)
+			{
+				v = round(n[0][t].getSol());
+				cout << t << ": " << v;
+				file << "n_0_" << t << ": " << v << endl;;
+
+				for (r = 1; r < NRES; ++r)
+				{
+					v = round(n[r][t].getSol());
+					cout << ", " << v;
+					file << "n_" << r << "_" << t << ": " << v << endl;;
+				}
+				cout << endl;
+			}
+		}
 	}
 
 	cout << "Start and finish time per asset and task: " << endl;
