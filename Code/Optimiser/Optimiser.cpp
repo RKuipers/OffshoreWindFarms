3d03ffd59b44e4f12643262696e267ee240e819d
RKuipers <robin_kuipers93@hotmail.com>
2020-06-05 08:46:50 +0100

    Updated every constr gen with rels

--- Code/Optimiser/Optimiser.cpp
+++ Code/Optimiser/Optimiser.cpp
@@ -462,31 +462,25 @@ private:
 		for (int a = 0; a < NASSETS; ++a)
 			for (int i = 0; i < NTASKS; ++i)
 			{
+				XPRBrelation rs = s[a][i][0] == 1;
+				XPRBrelation rf = f[a][i][0] == 1;
+
+				for (int t = 1; t < NTIMES; ++t)
+				{
+					rs.addTerm(s[a][i][t]);
+					rf.addTerm(f[a][i][t]);
+				}
+
 				if (cut)
 				{
-					XPRBcut cuts, cutf;
-					int indices[2] = { a, i };
-					cuts = prob->newCut(s[a][i][0] == 1);
-						cutf = prob->newCut(f[a][i][0] == 1);
-
-					for (int t = 1; t < NTIMES; ++t)
-					{
-						cuts.addTerm(s[a][i][t]);
-						cutf.addTerm(f[a][i][t]);
-					}
+					prob->newCut(rs);
+					prob->newCut(rf);
 				}
 				else
 				{
-					XPRBctr ctrs, ctrf;
 					int indices[2] = { a, i };
-					ctrs = genCon(prob, s[a][i][0] == 1, "Sta", 2, indices);
-						ctrf = genCon(prob, f[a][i][0] == 1, "Fin", 2, indices);
-
-					for (int t = 1; t < NTIMES; ++t)
-					{
-						ctrs.addTerm(s[a][i][t]);
-						ctrf.addTerm(f[a][i][t]);
-					}
+					genCon(prob, rs, "Sta", 2, indices);
+					genCon(prob, rf, "Fin", 2, indices);
 				}
 			}
 	}
@@ -527,11 +521,13 @@ private:
 						for (int t3 = t1; t3 <= t2; ++t3)
 							weather += OMEGA[i][t3];
 
+						XPRBrelation rel = (f[a][i][t2] + s[a][i][t1]) * 0.5 * weather >= d[i] * (s[a][i][t1] + f[a][i][t2] - 1);
+
 						int indices[4] = { a, i, t1, t2 };
 						if (cut)
-							prob->newCut((f[a][i][t2] + s[a][i][t1]) * 0.5 * weather >= d[i] * (s[a][i][t1] + f[a][i][t2] - 1));
+							prob->newCut(rel);
 						else
-							genCon(prob, (f[a][i][t2] + s[a][i][t1]) * 0.5 * weather >= d[i] * (s[a][i][t1] + f[a][i][t2] - 1), "Dur", 4, indices);
+							genCon(prob, rel, "Dur", 4, indices);
 					}
 				}
 
