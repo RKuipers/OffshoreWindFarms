545a613ff51ed17e930656632d33c0431d222cd9
RKuipers <robin_kuipers93@hotmail.com>
2020-11-16 13:46:10 +0000

    Inputdata constructors

--- Code/Overlap/InputData.cpp
+++ Code/Overlap/InputData.cpp
@@ -1,8 +1,33 @@
 #include "InputData.h"
 
+YearData::YearData(int s, int m, int y, int ip, int ir) : S(s), M(m), Y(y), Ip(ip), Ir(ir)
+{
+	c = vector<vector<double>>(Y, vector<double>(M, 0.0));
+	eH = vector<double>(M, 0.0);
+	f = vector<vector<vector<int>>>(M, vector<vector<int>>(Ir, vector<int>(S, 0)));
+	H = vector<int>(M, 0);
+	L = vector<int>(Y, 0);
+	LInst = vector<vector<int>>(Y, vector<int>(M, 0));
+	dPy = vector<double>(Y, 0.0);
+	dR = vector<vector<double>>(Y, vector<double>(Ir, 0.0));
+	A = vector<vector<int>>(Y, vector<int>(M, 0));
+	NInst = vector<vector<int>>(Y, vector<int>(M, 0));
+}
+
 YearData* YearData::getYear()
 {
 	return this;
+}
+
+MonthData::MonthData(int y, int v, int i, int ii, int j) : Y(y), V(v), I(i), IInst(ii), J(j)
+{
+	Vy = vector<int>(M, 0);
+	c = vector<double>(I, 0.0);
+	s = vector<vector<double>>(Y, vector<double>(I, 0.0));
+	d = vector<vector<double>>(Y, vector<double>(I, 0.0));
+	rho = vector<vector<int>>(Y, vector<int>(I, 0));
+	sInst = vector<double>(I, 0.0);
+	aInst = vector<vector<int>>(V, vector<int>(I, 0));
 }
 
 MonthData* MonthData::getMonth()
