466e396b7d6f42b285d12057a46203fb5dbd9110
RKuipers <robin_kuipers93@hotmail.com>
2020-11-17 14:22:54 +0000

    Update DataGen.h

--- Code/Overlap/DataGen.h
+++ Code/Overlap/DataGen.h
@@ -13,6 +13,7 @@ private:
 	vector<string> readLine(ifstream* datafile, char sep = '\t');
 	bool readEmpty(ifstream* datafile, char sep = '\t');
 	int parseArray(vector<string> line, int start, vector<int>* res, int amount);
+	int parseArrayDouble(vector<string> line, int start, vector<double>* res, int amount);
 
 public:
 	YearData readYear(ifstream* file);
