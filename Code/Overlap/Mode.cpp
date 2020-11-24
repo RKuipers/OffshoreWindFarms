57eb8280c394c2c3c1eeaad93d747cfccf39375b
RKuipers <robin_kuipers93@hotmail.com>
2020-11-24 17:00:39 +0000

    Update Mode.cpp

--- Code/Overlap/Mode.cpp
+++ Code/Overlap/Mode.cpp
@@ -1,11 +1,55 @@
 #include "Mode.h"
 
+//----------------------------------------------MODEDIM--------------------------------------------
+
+ModeDim::ModeDim(vector<string> names) : names(names), max(names.size()) {}
+
+int ModeDim::next()
+{
+	current++;
+	if (current >= max)
+	{
+		current = 0;
+		return -1;
+	}
+	return current;
+}
+
+string ModeDim::getCurrent()
+{
+	return names[current];
+}
+
+int ModeDim::getCurrentId()
+{
+	return current;
+}
+
+//--------------------------------------------MODEDIMCOMB------------------------------------------
+
+int ModeDimComb::next()
+{
+	return 0; // TODO
+}
+
+//-----------------------------------------------MODE----------------------------------------------
+
+int Mode::getCurrent(string name)
+{
+	return 0; // TODO
+}
+
 string Mode::getCurrentName()
 {
 	return "test"; // TODO
 }
 
 int Mode::getCurrentId()
+{
+	return 0; // TODO
+}
+
+int Mode::next()
 {
 	return 0; // TODO
 }
