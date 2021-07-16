f99619a74e5bc6231d17d600edc8d1ded5693506
RKuipers <robin_kuipers93@hotmail.com>
2021-07-16 15:08:12 +0100

    R & Testcases

--- R/plotting.R
+++ R/plotting.R
@@ -1,47 +1,65 @@
 #Import data
 resultsRaw <- read.csv2(file = "aaa_Collective.csv")
 colnames(resultsRaw) <- colnames(resultsRaw[-1])
-resultsRaw <- subset(resultsRaw, select = -15)
+resultsRaw <- subset(resultsRaw, select = -c(2, 15))
 
-#Set data category
+#Create Duration list
+durList = list()
+durList[unique(resultsRaw$Dur)] <- c(24, 6, 12, 21, 18, 33, 30)
+conv <- function(dur) {return(durList$dur)}
+
+#Set data category and total
 library(dplyr)
 resultsRaw <- resultsRaw %>% mutate(Category = ifelse(Turbines == 'Al', 'Sep', ifelse(Vessels == 'NO', 'Ove', 'Sha')))
-colnames(resultsRaw) <- c("Objective", "Duration", "Avail time", "Avail ener", "ProdLoss", "DirCosts", "VesCosts", "RepCosts", "TechCosts", "Turbines", "Dur", "Vessels", "Percent", "Size", "Category")
+dur.names = resultsRaw$Dur
+resultsRaw$Dur <- unlist(durList[resultsRaw$Dur])
+resultsRaw$Total <- (resultsRaw$Objective / resultsRaw$Dur) * 12
+colnames(resultsRaw) <- c("Objective", "Avail time", "Avail ener", "ProdLoss", "DirCosts", "VesCosts", "RepCosts", "TechCosts", "Turbines", "Dur", "Vessels", "Percent", "Size", "Category", "Total")
 
 #Plot
 library(ggplot2)
 library(GGally)
-resultsAgg <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category, resultsRaw, mean)
+resultsAgg <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category, resultsRaw, mean)
 ggparcoord(resultsAgg, columns = c(2, 3, 4, 5, 6, 7), groupColumn = "Category", scale = "globalminmax") +
   geom_line(size = 1.5)
 
+#Plot function
+myplot <- function(df, group1 = "Category", group2 = NULL)
+{
+  ggparcoord(df, columns = c(3, 4, 5, 6, 7, 8), groupColumn = group1, scale = "globalminmax") +
+    geom_line(size = 1) + 
+    facet_wrap(group2, scales = "free")
+}
+
 #Breakdown plots
 library(crayon)
-resultsDur <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Dur, resultsRaw, mean)
+resultsDur <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Dur, resultsRaw, mean)
 ggparcoord(resultsDur, columns = c(3, 4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
   geom_line(aes(lty = as.factor(Dur)), size = 1.5) + 
   scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "blank", "dotdash", "dashed"))
+myplot(resultsDur, group2 = "Dur")
 
-resultsTurb <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Turbines, resultsRaw, mean)
+resultsTurb <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Turbines, resultsRaw, mean)
 ggparcoord(resultsTurb, columns = c(3, 4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
   geom_line(aes(lty = as.factor(Turbines)), size = 1.5) + 
   scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "blank", "dotdash", "dashed"))
+myplot(resultsTurb, group2 = "Turbines")
 
-resultsVes <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Vessels, resultsRaw, mean)
-ggparcoord(resultsVes, columns = c(4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
-  geom_line(aes(lty = as.factor(Vessels)), size = 1.5) + 
-  scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "dotdash", "dashed"))
+resultsVes <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Vessels, resultsRaw, mean)
+#ggparcoord(resultsVes, columns = c(4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
+  #geom_line(aes(lty = as.factor(Vessels)), size = 1.5) + 
+  #scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "dotdash", "dashed"))
+myplot(resultsVes)
 
-resultsPerc <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Percent, resultsRaw, mean) %>% transform(Percent = chr(Percent))
-ggparcoord(resultsPerc, columns = c(4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
-  geom_line(aes(lty = as.factor(Percent)), size = 1.5) + 
-  scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "dotdash", "dashed"))
+resultsPerc <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Percent, resultsRaw, mean) %>% transform(Percent = chr(Percent))
+#ggparcoord(resultsPerc, columns = c(4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
+  #geom_line(aes(lty = as.factor(Percent)), size = 1.5) + 
+  #scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid", "dotdash", "dashed"))
+myplot(resultsPerc)
 
-resultsSize <- aggregate(cbind(Objective, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Size, resultsRaw, mean) %>% transform(Size = chr(Size))
-ggparcoord(resultsSize, columns = c(4, 5, 6, 7, 8), groupColumn = "Category", scale = "globalminmax") +
-  geom_line(aes(lty = as.factor(Size)), size = 1.5) + 
-  scale_linetype_manual(values=c("twodash", "dotted", "longdash", "solid"))
+resultsSize <- aggregate(cbind(Total, ProdLoss, DirCosts, VesCosts, RepCosts, TechCosts) ~ Category + Size, resultsRaw, mean)
+myplot(resultsSize, "Category", "Size")
 
 #TODO: Check out parcoords ipv ggparcoord
-#TODO: Transform Objective to Total (annual)
 #TODO: Create "normalized" dataframe which normalizes all data by #turbines
+#TODO: Make 2 plot functions: plotfacet (what I currently have, though can be extended with 3rd group (2nd facet)) and plotline (or other method to visualize second split) 
